67bc422e550b3647a1f959ffc18c286bd7593c03
Amirhajji <amir.hajji@esprit.tn>
2026-02-12 12:40:32 +0100

    allow frontvm from crossorigine

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,9 @@ from fastapi import status
 app = FastAPI(title="PlatformOPS Observability API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","http://127.0.0.1:5173" ,"http://localhost:3000",],
+    allow_origins=["http://localhost:5173","http://127.0.0.1:5173" ,"http://localhost:3000","http://192.168.21.134",
+        "http://192.168.21.134:5173",
+        "http://192.168.21.134:3000",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
